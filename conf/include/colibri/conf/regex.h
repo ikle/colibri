d03c5191145f678dbc648f3d910c1ea28a030135
Alexei A. Smekalkine <ikle@ikle.ru>
2021-09-10 08:30:43 +0300

    conf: resurrect regular expression matcher from cm-kind history

---
+++ conf/include/colibri/conf/regex.h
@@ -0,0 +1,14 @@
+/*
+ * Colibri regular expression helpers
+ *
+ * Copyright (c) 2011-2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef COLIBRI_CONF_REGEX_H
+#define COLIBRI_CONF_REGEX_H
+
+int regex_match (const char *re, const char *value);
+
+#endif  /* COLIBRI_CONF_REGEX_H */
---
+++ conf/regex.c
@@ -0,0 +1,27 @@
+/*
+ * Colibri regular expression helpers
+ *
+ * Copyright (c) 2011-2021 Alexei A. Smekalkine <ikle@ikle.ru>
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <stddef.h>
+
+#include <regex.h>
+
+#include <colibri/conf/regex.h>
+
+int regex_match (const char *re, const char *value)
+{
+	regex_t preg;
+	int ret;
+
+	if (regcomp (&preg, re, REG_EXTENDED | REG_NOSUB) != 0)
+		return 0;
+
+	ret = regexec (&preg, value, 0, NULL, 0) == 0;
+
+	regfree (&preg);
+	return ret;
+}
